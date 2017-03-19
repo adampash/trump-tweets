4e28a3cde989574da726a5f52d6edcf2b71ba3af
Adam Pash <adam.pash@gmail.com>
2017-03-19 12:50:22 -0700

    trying prettier

--- src/hello.js
+++ src/hello.js
@@ -8,10 +8,8 @@ const hello = (event, context, callback) => {
   };
 
   callback(null, response);
-
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
 
 export default hello;
-
---
+++ src/intents.js
@@ -0,0 +1,2 @@
+export const DID_TRUMP_TWEET = 'DID_TRUMP_TWEET';
+export const READ_TWEETS = 'READ_TWEETS';
